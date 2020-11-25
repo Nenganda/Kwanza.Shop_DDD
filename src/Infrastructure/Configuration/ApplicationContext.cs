aade1d423d4032a13328d160ead6cdd7633252d7
Nenganda Francisco <nenganda@hotmail.com>
2020-11-25 20:11:39 +0100

    4 - Infra  - Config. Interfaces e Repository  - Add Referencias

---
+++ src/Infrastructure/Configuration/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) :  base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=(localdb)\\mssqllocaldb;Database=Kwanza.Shop;Trusted_Connection=True;MultipleActiveResultSets=true";
+            return strCon;
+        }
+    }
+}
